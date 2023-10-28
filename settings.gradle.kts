d75809423d473c5e4831e5add9de3ded32df31f4
vitekkor <viktor.korotkikh@gmail.com>
2023-10-28 22:16:37 +0300

    #11: create module for database models

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "automated-testing-applicants-service"
 include("exam-manage-service")
 include("exam-conduct-service")
 include("exam-judging-service")
+include("common:database")
