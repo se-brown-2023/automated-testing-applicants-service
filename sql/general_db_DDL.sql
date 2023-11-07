d137907a64d4634c4c5beba649100887de014a52
atveritin <atveritin@cinimex.ru>
2023-11-07 15:36:41 +0300

    - Правки порядка инциализации таблиц в DDL.

--- sql/general_db_DDL.sql
+++ sql/general_db_DDL.sql
@@ -1,28 +1,3 @@
-CREATE TABLE Test (
-    id SERIAL PRIMARY KEY,
-    task_id INTEGER REFERENCES Task(id),
-    name VARCHAR(70),
-    input_data TEXT,
-    expected_output_data TEXT
-);
-
-CREATE TABLE Task (
-    id SERIAL PRIMARY KEY,
-    exam_id INTEGER REFERENCES Exam(id),
-    name VARCHAR(70) NOT NULL,
-    description TEXT,
-    author_source_code TEXT
-);
-
-CREATE TABLE Test_result (
-    id SERIAL PRIMARY KEY,
-    submission_id INTEGER REFERENCES Submission(id),
-    test_id INTEGER REFERENCES Test(id),
-    actual_output_data TEXT,
-    passed BOOLEAN NOT NULL,
-    elapsed_time INTERVAL
-);
-
 CREATE TABLE Exam (
     id SERIAL PRIMARY KEY,
     examiner_id INTEGER,
@@ -34,12 +9,28 @@ CREATE TABLE Exam (
     creation_date TIMESTAMP
 );
 
-CREATE TABLE Submission (
+CREATE TABLE Task (
+    id SERIAL PRIMARY KEY,
+    exam_id INTEGER REFERENCES Exam(id),
+    name VARCHAR(70) NOT NULL,
+    description TEXT,
+    author_source_code TEXT
+);
+
+CREATE TABLE Test (
     id SERIAL PRIMARY KEY,
     task_id INTEGER REFERENCES Task(id),
-    exam_session_id INTEGER REFERENCES Exam_session(id),
-    user_source_code TEXT,
-    submit_time TIMESTAMP
+    name VARCHAR(70),
+    input_data TEXT,
+    expected_output_data TEXT
+);
+
+CREATE TABLE Examinee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(15) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    phone_number VARCHAR(15)
 );
 
 CREATE TABLE Exam_session (
@@ -52,10 +43,19 @@ CREATE TABLE Exam_session (
     expired BOOLEAN
 );
 
-CREATE TABLE Examinee (
+CREATE TABLE Submission (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(15) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    phone_number VARCHAR(15)
+    task_id INTEGER REFERENCES Task(id),
+    exam_session_id INTEGER REFERENCES Exam_session(id),
+    user_source_code TEXT,
+    submit_time TIMESTAMP
+);
+
+CREATE TABLE Test_result (
+    id SERIAL PRIMARY KEY,
+    submission_id INTEGER REFERENCES Submission(id),
+    test_id INTEGER REFERENCES Test(id),
+    actual_output_data TEXT,
+    passed BOOLEAN NOT NULL,
+    elapsed_time INTERVAL
 );
