36f7e834ddca791b9b8074190f3e906f63fc0760
vitekkor <viktor.korotkikh@gmail.com>
2023-11-20 15:52:34 +0300

    #33: add some base files

---
+++ exam-judging-service/src/main/java/com/sebrown2023/compiler/JavaCompiler.java
@@ -0,0 +1,7 @@
+package com.sebrown2023.compiler;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JavaCompiler {
+}
---
+++ exam-judging-service/src/main/java/com/sebrown2023/service/JudgeEngine.java
@@ -0,0 +1,4 @@
+package com.sebrown2023.service;
+
+public interface JudgeEngine {
+}
