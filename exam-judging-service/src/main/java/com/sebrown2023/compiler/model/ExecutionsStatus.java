290e15097354101c0caa2c7b858d5b9f7f0e1112
vitekkor <viktor.korotkikh@gmail.com>
2023-11-27 00:10:10 +0300

    #33: add Custom watchdogs

--- exam-judging-service/src/main/java/com/sebrown2023/compiler/model/ExecutionsStatus.java
+++ exam-judging-service/src/main/java/com/sebrown2023/compiler/model/ExecutionsStatus.java
@@ -2,6 +2,7 @@ package com.sebrown2023.compiler.model;
 
 public record ExecutionsStatus(
         String outputStream,
-        String errorStream
+        String errorStream,
+        long executionTimeMs
 ) {
 }
