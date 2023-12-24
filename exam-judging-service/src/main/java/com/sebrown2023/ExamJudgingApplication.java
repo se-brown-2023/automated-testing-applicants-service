a5e690160903248559321909e456840ca91e2e3c
vitekkor <viktor.korotkikh@gmail.com>
2023-12-24 04:21:01 +0300

    #38: fix Dev

--- exam-judging-service/src/main/java/com/sebrown2023/ExamJudgingApplication.java
+++ exam-judging-service/src/main/java/com/sebrown2023/ExamJudgingApplication.java
@@ -12,14 +12,13 @@ public class ExamJudgingApplication {
 
     public static class Dev {
         public static void main(String[] args) {
-
+            System.setProperty("spring.profiles.active", "dev,local,local-s3");
+            System.setProperty("log.dir", "exam-judging-service/logs");
             ExamJudgingApplication.main(args);
         }
     }
 
     public static void main(String[] args) {
-        System.setProperty("spring.profiles.active", "dev,local,local-s3");
-        System.setProperty("log.dir", "exam-judging-service/logs");
         SpringApplication.run(ExamJudgingApplication.class, args);
     }
 }
