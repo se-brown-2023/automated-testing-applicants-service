ca6319cd40775a99d8718dc55afe0758dd4638c8
atveritin <atveritin@cinimex.ru>
2024-01-12 02:18:52 +0300

    - add requests and exceptions logging.

--- exam-manage-service/src/main/java/com/sebrown2023/exceptions/ApiExceptionHandler.java
+++ exam-manage-service/src/main/java/com/sebrown2023/exceptions/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.sebrown2023.exceptions;
 
 import com.sebrown2023.model.dto.ApiError;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @ControllerAdvice
 public class ApiExceptionHandler {
 
@@ -25,6 +27,12 @@ public class ApiExceptionHandler {
         responseError.setMessage(handledEx.getMessage());
         responseError.setRequest(requestInfo);
         responseError.setTime(LocalDateTime.now().toString());
+
+        log.error("Tittle: {}, message: {}, requset: {}, time: {}",
+                responseError.getTitle(),
+                responseError.getMessage(),
+                responseError.getRequest(),
+                responseError.getTime());
 
         return new ResponseEntity<>(responseError, HttpStatus.BAD_REQUEST);
     }
