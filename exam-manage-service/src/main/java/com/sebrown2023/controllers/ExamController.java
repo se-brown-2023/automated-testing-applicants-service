0f0afbdee0ad155d72e6cd6a1576f118e24a17cb
axothy <globaltouareg@gmail.com>
2024-01-13 12:08:10 +0300

    secured rest endpoints

--- exam-manage-service/src/main/java/com/sebrown2023/controllers/ExamController.java
+++ exam-manage-service/src/main/java/com/sebrown2023/controllers/ExamController.java
@@ -3,6 +3,7 @@ package com.sebrown2023.controllers;
 import com.sebrown2023.controller.ExamApi;
 import com.sebrown2023.model.dto.ExamComponent;
 import com.sebrown2023.services.ExamService;
+import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,36 +16,42 @@ public class ExamController implements ExamApi {
     private final ExamService examService;
 
     @Override
+    @RolesAllowed({"ROLE_ADMIN"})
     public ResponseEntity<ExamComponent> getExam(Long examId) {
         var examComponent = examService.getExamComponentById(examId);
         return ResponseEntity.ok(examComponent);
     }
 
     @Override
+    @RolesAllowed({"ROLE_ADMIN"})
     public ResponseEntity<List<ExamComponent>> getAllExams() {
         var examComponents = examService.getAllExamWithTasksAndTests();
         return ResponseEntity.ok(examComponents);
     }
 
     @Override
+    @RolesAllowed({"ROLE_ADMIN"})
     public ResponseEntity<List<ExamComponent>> getExamsForExaminer(Long examinerId) {
         var examComponents = examService.getAllExamWithTasksAndTestsByExaminerId(examinerId);
         return ResponseEntity.ok(examComponents);
     }
 
     @Override
+    @RolesAllowed({"ROLE_ADMIN"})
     public ResponseEntity<ExamComponent> addTasksToExam(Long examId, List<Long> tasksIds) {
         var examComponent = examService.addTasksToExam(examId, tasksIds);
         return ResponseEntity.ok(examComponent);
     }
 
     @Override
+    @RolesAllowed({"ROLE_ADMIN"})
     public ResponseEntity<ExamComponent> createExam(ExamComponent examComponent) {
         var createdExamComponent = examService.createExam(examComponent);
         return ResponseEntity.ok().body(createdExamComponent);
     }
 
     @Override
+    @RolesAllowed({"ROLE_ADMIN"})
     public ResponseEntity<Void> deleteExam(Long examId) {
         examService.deleteExam(examId);
         return ResponseEntity.ok().build();
--- exam-manage-service/src/main/java/com/sebrown2023/controllers/TaskController.java
+++ exam-manage-service/src/main/java/com/sebrown2023/controllers/TaskController.java
@@ -3,6 +3,7 @@ package com.sebrown2023.controllers;
 import com.sebrown2023.controller.TaskApi;
 import com.sebrown2023.model.dto.TaskComponent;
 import com.sebrown2023.services.TaskService;
+import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,30 +16,35 @@ public class TaskController implements TaskApi {
     private final TaskService taskService;
 
     @Override
+    @RolesAllowed({"ROLE_ADMIN"})
     public ResponseEntity<TaskComponent> getTask(Long taskId) {
         var taskComponent = taskService.getTaskComponentById(taskId);
         return ResponseEntity.ok(taskComponent);
     }
 
     @Override
+    @RolesAllowed({"ROLE_ADMIN"})
     public ResponseEntity<List<TaskComponent>> getAllTasks() {
         var taskComponents = taskService.getAllTaskComponents();
         return ResponseEntity.ok(taskComponents);
     }
 
     @Override
+    @RolesAllowed({"ROLE_ADMIN"})
     public ResponseEntity<List<TaskComponent>> getTasksForExam(Long examId) {
         var taskComponents = taskService.getAllTaskComponentsByExamId(examId);
         return ResponseEntity.ok(taskComponents);
     }
 
     @Override
+    @RolesAllowed({"ROLE_ADMIN"})
     public ResponseEntity<TaskComponent> createTask(TaskComponent taskComponent) {
         var createdTaskComponent = taskService.createTask(taskComponent);
         return ResponseEntity.ok().body(createdTaskComponent);
     }
 
     @Override
+    @RolesAllowed({"ROLE_ADMIN"})
     public ResponseEntity<Void> deleteTask(Long taskId) {
         taskService.deleteTask(taskId);
         return ResponseEntity.ok().build();
